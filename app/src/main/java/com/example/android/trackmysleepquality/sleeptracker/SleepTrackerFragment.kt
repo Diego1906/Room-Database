3a44a4c42ae61583f419905cbba118e3d3e374a8
Diego <di_fsantos@yahoo.com.br>
2020-01-16 17:54:59 -0300

    Ajustes na classe "SleepTrackerFragment" - Criação do ViewModel e ViewModelFactory - Inicialização da variável viewModel do databinding - Definição do proprietário do ciclo de vida do databinding
    Implementação da variável viewModel no bloco data> do xml "fragment_sleep_tracker"

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -22,7 +22,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDataBase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
 
 /**
@@ -41,9 +43,24 @@ class SleepTrackerFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         // Get a reference to the binding object and inflate the fragment views.
-        val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
+        val dataBinding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_sleep_tracker, container, false)
 
-        return binding.root
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDataBase.getInstance(application).sleepDatabaseDao
+
+        // Create an instance of the ViewModel Factory.
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        // Get a reference to the ViewModel associated with this fragment.
+        val sleepTrackerViewModel = ViewModelProviders.of(this, viewModelFactory)
+                .get(SleepTrackerViewModel::class.java)
+
+        dataBinding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        dataBinding.setLifecycleOwner(this)
+
+        return dataBinding.root
     }
 }
